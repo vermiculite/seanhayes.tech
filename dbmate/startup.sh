35edc3a3424762855631cd33b9eb477f006bbee7
Sean Hayes <seanowenhayes@gmail.com>
2023-05-18 20:51:38 +0200

    Start afresh with only yew (pun intended)

--- dbmate/startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dbmate wait
-dbmate up
--- types/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct HelloResponse {
-    pub name: String,
-}
